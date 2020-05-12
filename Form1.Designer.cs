e8ba617c91ae8394c7680038766846626d32663a
Rey Castro <reycsa95@gmail.com>
2020-05-12 13:59:40 -0500

    Programa de una calculadora basica

---
+++ Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace RACSAcalculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.txb = new System.Windows.Forms.TextBox();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnMas = new System.Windows.Forms.Button();
+            this.btnMenos = new System.Windows.Forms.Button();
+            this.btnMtp = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnIgual = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(44, 164);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(42, 23);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            this.btn1.MouseEnter += new System.EventHandler(this.btn1_MouseEnter);
+            this.btn1.MouseLeave += new System.EventHandler(this.btn1_MouseLeave);
+            // 
+            // txb
+            // 
+            this.txb.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb.Location = new System.Drawing.Point(55, 25);
+            this.txb.MaxLength = 15;
+            this.txb.Name = "txb";
+            this.txb.Size = new System.Drawing.Size(196, 30);
+            this.txb.TabIndex = 1;
+            this.txb.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txb.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txb_KeyPress);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(102, 164);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(42, 23);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            this.btn2.MouseEnter += new System.EventHandler(this.btn2_MouseEnter);
+            this.btn2.MouseLeave += new System.EventHandler(this.btn2_MouseLeave);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(160, 164);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(42, 23);
+            this.btn3.TabIndex = 3;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            this.btn3.MouseEnter += new System.EventHandler(this.btn3_MouseEnter);
+            this.btn3.MouseLeave += new System.EventHandler(this.btn3_MouseLeave);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(44, 125);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(42, 23);
+            this.btn4.TabIndex = 4;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            this.btn4.MouseEnter += new System.EventHandler(this.btn4_MouseEnter);
+            this.btn4.MouseLeave += new System.EventHandler(this.btn4_MouseLeave);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(102, 125);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(42, 23);
+            this.btn5.TabIndex = 5;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            this.btn5.MouseEnter += new System.EventHandler(this.btn5_MouseEnter);
+            this.btn5.MouseLeave += new System.EventHandler(this.btn5_MouseLeave);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(160, 125);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(42, 23);
+            this.btn6.TabIndex = 6;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            this.btn6.MouseEnter += new System.EventHandler(this.btn6_MouseEnter);
+            this.btn6.MouseLeave += new System.EventHandler(this.btn6_MouseLeave);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(44, 87);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(42, 23);
+            this.btn7.TabIndex = 7;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            this.btn7.MouseEnter += new System.EventHandler(this.btn7_MouseEnter);
+            this.btn7.MouseLeave += new System.EventHandler(this.btn7_MouseLeave);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(102, 87);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(42, 23);
+            this.btn8.TabIndex = 8;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            this.btn8.MouseEnter += new System.EventHandler(this.btn8_MouseEnter);
+            this.btn8.MouseLeave += new System.EventHandler(this.btn8_MouseLeave);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(160, 87);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(42, 23);
+            this.btn9.TabIndex = 9;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            this.btn9.MouseEnter += new System.EventHandler(this.btn9_MouseEnter);
+            this.btn9.MouseLeave += new System.EventHandler(this.btn9_MouseLeave);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(44, 203);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(42, 23);
+            this.btn0.TabIndex = 10;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            this.btn0.MouseEnter += new System.EventHandler(this.btn0_MouseEnter);
+            this.btn0.MouseLeave += new System.EventHandler(this.btn0_MouseLeave);
+            // 
+            // btnMas
+            // 
+            this.btnMas.Location = new System.Drawing.Point(219, 203);
+            this.btnMas.Name = "btnMas";
+            this.btnMas.Size = new System.Drawing.Size(42, 23);
+            this.btnMas.TabIndex = 11;
+            this.btnMas.Text = "+";
+            this.btnMas.UseVisualStyleBackColor = true;
+            this.btnMas.Click += new System.EventHandler(this.btnMas_Click);
+            this.btnMas.MouseEnter += new System.EventHandler(this.btnMas_MouseEnter);
+            this.btnMas.MouseLeave += new System.EventHandler(this.btnMas_MouseLeave);
+            // 
+            // btnMenos
+            // 
+            this.btnMenos.Location = new System.Drawing.Point(219, 164);
+            this.btnMenos.Name = "btnMenos";
+            this.btnMenos.Size = new System.Drawing.Size(42, 23);
+            this.btnMenos.TabIndex = 12;
+            this.btnMenos.Text = "-";
+            this.btnMenos.UseVisualStyleBackColor = true;
+            this.btnMenos.Click += new System.EventHandler(this.btnMenos_Click);
+            this.btnMenos.MouseEnter += new System.EventHandler(this.btnMenos_MouseEnter);
+            this.btnMenos.MouseLeave += new System.EventHandler(this.btnMenos_MouseLeave);
+            // 
+            // btnMtp
+            // 
+            this.btnMtp.Location = new System.Drawing.Point(219, 125);
+            this.btnMtp.Name = "btnMtp";
+            this.btnMtp.Size = new System.Drawing.Size(42, 23);
+            this.btnMtp.TabIndex = 13;
+            this.btnMtp.Text = "*";
+            this.btnMtp.UseVisualStyleBackColor = true;
+            this.btnMtp.Click += new System.EventHandler(this.btnMtp_Click);
+            this.btnMtp.MouseEnter += new System.EventHandler(this.btnMtp_MouseEnter);
+            this.btnMtp.MouseLeave += new System.EventHandler(this.btnMtp_MouseLeave);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(219, 87);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(42, 23);
+            this.btnDiv.TabIndex = 14;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            this.btnDiv.MouseEnter += new System.EventHandler(this.btnDiv_MouseEnter);
+            this.btnDiv.MouseLeave += new System.EventHandler(this.btnDiv_MouseLeave);
+            // 
+            // btnIgual
+            // 
+            this.btnIgual.Location = new System.Drawing.Point(102, 203);
+            this.btnIgual.Name = "btnIgual";
+            this.btnIgual.Size = new System.Drawing.Size(100, 23);
+            this.btnIgual.TabIndex = 15;
+            this.btnIgual.Text = "=";
+            this.btnIgual.UseVisualStyleBackColor = true;
+            this.btnIgual.Click += new System.EventHandler(this.btnIgual_Click);
+            this.btnIgual.MouseEnter += new System.EventHandler(this.btnIgual_MouseEnter);
+            this.btnIgual.MouseLeave += new System.EventHandler(this.btnIgual_MouseLeave);
+            // 
+            // btnDel
+            // 
+            this.btnDel.ForeColor = System.Drawing.Color.Crimson;
+            this.btnDel.Location = new System.Drawing.Point(219, 58);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(42, 23);
+            this.btnDel.TabIndex = 16;
+            this.btnDel.Text = "C";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            this.btnDel.MouseEnter += new System.EventHandler(this.btnDel_MouseEnter);
+            this.btnDel.MouseLeave += new System.EventHandler(this.btnDel_MouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnIgual);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMtp);
+            this.Controls.Add(this.btnMenos);
+            this.Controls.Add(this.btnMas);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.txb);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "RACSAcalculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox txb;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btnMas;
+        private System.Windows.Forms.Button btnMenos;
+        private System.Windows.Forms.Button btnMtp;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnIgual;
+        private System.Windows.Forms.Button btnDel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,398 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace RACSAcalculadora
+{
+    public partial class Form1 : Form
+    {
+        bool click = true;
+        string accion;
+        double x, y, total;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            if (txb.Text == "0")
+            {
+                return;
+            }
+            else
+            {
+                txb.Text = txb.Text + "0";
+            }
+        }
+
+        private void btn0_MouseEnter(object sender, EventArgs e)
+        {
+            this.btn0.BackColor = Color.Gold;
+        }
+
+        private void btn0_MouseLeave(object sender, EventArgs e)
+        {
+            this.btn0.BackColor = SystemColors.Control;
+        }
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (click)
+            {
+                txb.Text = "";
+                txb.Text = "1";
+                click = false;
+            }
+            else
+            {
+                txb.Text = txb.Text + "1";
+            }
+        }
+        private void btn1_MouseEnter(object sender, EventArgs e)
+        {
+            this.btn1.BackColor = Color.Gold;
+        }
+
+        private void btn1_MouseLeave(object sender, EventArgs e)
+        {
+            this.btn1.BackColor = SystemColors.Control;
+        }
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (click)
+            {
+                txb.Text = "";
+                txb.Text = "2";
+                click = false;
+            }
+            else
+            {
+                txb.Text = txb.Text + "2";
+            }
+        }
+        private void btn2_MouseEnter(object sender, EventArgs e)
+        {
+            this.btn2.BackColor = Color.Gold;
+        }
+
+        private void btn2_MouseLeave(object sender, EventArgs e)
+        {
+            this.btn2.BackColor = SystemColors.Control;
+        }
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (click)
+            {
+                txb.Text = "";
+                txb.Text = "3";
+                click = false;
+            }
+            else
+            {
+                txb.Text = txb.Text + "3";
+            }
+        }
+        private void btn3_MouseEnter(object sender, EventArgs e)
+        {
+            this.btn3.BackColor = Color.Gold;
+        }
+
+        private void btn3_MouseLeave(object sender, EventArgs e)
+        {
+            this.btn3.BackColor = SystemColors.Control;
+        }
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (click)
+            {
+                txb.Text = "";
+                txb.Text = "4";
+                click = false;
+            }
+            else
+            {
+                txb.Text = txb.Text + "4";
+            }
+        }
+        private void btn4_MouseEnter(object sender, EventArgs e)
+        {
+            this.btn4.BackColor = Color.Gold;
+        }
+
+        private void btn4_MouseLeave(object sender, EventArgs e)
+        {
+            this.btn4.BackColor = SystemColors.Control;
+        }
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (click)
+            {
+                txb.Text = "";
+                txb.Text = "5";
+                click = false;
+            }
+            else
+            {
+                txb.Text = txb.Text + "5";
+            }
+        }
+        private void btn5_MouseEnter(object sender, EventArgs e)
+        {
+            this.btn5.BackColor = Color.Gold;
+        }
+
+        private void btn5_MouseLeave(object sender, EventArgs e)
+        {
+            this.btn5.BackColor = SystemColors.Control;
+        }
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (click)
+            {
+                txb.Text = "";
+                txb.Text = "6";
+                click = false;
+            }
+            else
+            {
+                txb.Text = txb.Text + "6";
+            }
+        }
+        private void btn6_MouseEnter(object sender, EventArgs e)
+        {
+            this.btn6.BackColor = Color.Gold;
+        }
+
+        private void btn6_MouseLeave(object sender, EventArgs e)
+        {
+            this.btn6.BackColor = SystemColors.Control;
+        }
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (click)
+            {
+                txb.Text = "";
+                txb.Text = "7";
+                click = false;
+            }
+            else
+            {
+                txb.Text = txb.Text + "7";
+            }
+        }
+        private void btn7_MouseEnter(object sender, EventArgs e)
+        {
+            this.btn7.BackColor = Color.Gold;
+        }
+
+        private void btn7_MouseLeave(object sender, EventArgs e)
+        {
+            this.btn7.BackColor = SystemColors.Control;
+        }
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (click)
+            {
+                txb.Text = "";
+                txb.Text = "8";
+                click = false;
+            }
+            else
+            {
+                txb.Text = txb.Text + "8";
+            }
+        }
+        private void btn8_MouseEnter(object sender, EventArgs e)
+        {
+            this.btn8.BackColor = Color.Gold;
+        }
+
+        private void btn8_MouseLeave(object sender, EventArgs e)
+        {
+            this.btn8.BackColor = SystemColors.Control;
+        }
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (click)
+            {
+                txb.Text = "";
+                txb.Text = "9";
+                click = false;
+            }
+            else
+            {
+                txb.Text = txb.Text + "9";
+            }
+        }
+        private void btn9_MouseEnter(object sender, EventArgs e)
+        {
+            this.btn9.BackColor = Color.Gold;
+        }
+
+        private void btn9_MouseLeave(object sender, EventArgs e)
+        {
+            this.btn9.BackColor = SystemColors.Control;
+        }
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            txb.Clear();
+        }
+        private void btnDel_MouseEnter(object sender, EventArgs e)
+        {
+            this.btnDel.BackColor = Color.Gold;
+        }
+
+        private void btnDel_MouseLeave(object sender, EventArgs e)
+        {
+            this.btnDel.BackColor = SystemColors.Control;
+        }
+        private void btnMas_Click(object sender, EventArgs e)
+        {
+            accion = "+";
+            click = true;
+
+            x = double.Parse(txb.Text);
+        }
+        private void btnMas_MouseEnter(object sender, EventArgs e)
+        {
+            this.btnMas.BackColor = Color.Gold;
+        }
+
+        private void btnMas_MouseLeave(object sender, EventArgs e)
+        {
+            this.btnMas.BackColor = SystemColors.Control;
+        }
+        private void btnMenos_Click(object sender, EventArgs e)
+        {
+            accion = "-";
+            click = true;
+
+            x = double.Parse(txb.Text);
+        }
+        private void btnMenos_MouseEnter(object sender, EventArgs e)
+        {
+            this.btnMenos.BackColor = Color.Gold;
+        }
+
+        private void btnMenos_MouseLeave(object sender, EventArgs e)
+        {
+            this.btnMenos.BackColor = SystemColors.Control;
+        }
+        private void btnMtp_Click(object sender, EventArgs e)
+        {
+            accion = "*";
+            click = true;
+
+            x = double.Parse(txb.Text);
+        }
+        private void btnMtp_MouseEnter(object sender, EventArgs e)
+        {
+            this.btnMtp.BackColor = Color.Gold;
+        }
+
+        private void btnMtp_MouseLeave(object sender, EventArgs e)
+        {
+            this.btnMtp.BackColor = SystemColors.Control;
+        }
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            accion = "/";
+            click = true;
+
+            x = double.Parse(txb.Text);
+        }
+        private void btnDiv_MouseEnter(object sender, EventArgs e)
+        {
+            this.btnDiv.BackColor = Color.Gold;
+        }
+
+        private void btnDiv_MouseLeave(object sender, EventArgs e)
+        {
+            this.btnDiv.BackColor = SystemColors.Control;
+        }
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            y = double.Parse(txb.Text);
+
+            click = true;
+
+            if (accion == "+")
+            {
+                total = x + y;
+                txb.Text = total.ToString();
+                click = true;
+            }
+
+            if (accion == "-")
+            {
+                total = x - y;
+                txb.Text = total.ToString();
+                click = true;
+            }
+
+            if (accion == "*")
+            {
+                total = x * y;
+                txb.Text = total.ToString();
+                click = true;
+            }
+
+            if (accion == "/")
+            {
+                total = x / y;
+                txb.Text = total.ToString();
+                click = true;
+            }     
+                   
+        }
+        private void btnIgual_MouseEnter(object sender, EventArgs e)
+        {
+            this.btnIgual.BackColor = Color.Gold;
+        }
+
+
+        private void btnIgual_MouseLeave(object sender, EventArgs e)
+        {
+            this.btnIgual.BackColor = SystemColors.Control;
+        }
+        private void txb_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar)))
+            {
+                e.Handled = true;
+            }
+
+            char tecla = e.KeyChar;
+
+            switch (tecla)
+            {
+                case '+':
+                    btnMas.PerformClick();
+                    txb.Text = "";
+                    this.btnMas.BackColor = Color.Gold;
+                    break;
+                case '-':
+                    btnMenos.PerformClick();
+                    txb.Text = "";
+                    this.btnMas.BackColor = Color.Gold;
+                    break;
+                case '*':
+                    btnMtp.PerformClick();
+                    txb.Text = "";
+                    this.btnMas.BackColor = Color.Gold;
+                    break;
+                case '/':
+                    btnDiv.PerformClick();
+                    txb.Text = "";
+                    this.btnMas.BackColor = Color.Gold;
+                    break;
+            }
+
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = false;
+                btnIgual.PerformClick();
+            }                
+        }
+    }
+}
